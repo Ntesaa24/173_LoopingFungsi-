fe2e781bb7377c9d97b41cd6f10bac3815450ab2
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-03-19 10:53:02 +0700

    Looping While

--- Looping/Looping.cpp
+++ Looping/Looping.cpp
@@ -2,22 +2,25 @@
 using namespace std; 
 
 int main() {
-	
-	int i; 
-	int arr[5]; 
+
+	int i;
+	int arr[5];
 
 	for (i = 60; i > 10; i -= 10) {
 		cout << i << " Selamat PAgi Dunia" << endl;
 	}
 
-	cout << "Nilai i terakhir : " << i << endl; 
+	cout << "Nilai i terakhir : " << i << endl;
 
-	for (i = 0; i < 5;  i++) {
+	for (i = 0; i < 5; i++) {
 		cout << i << " Selamat Pagi Dunia" << endl;
-		cin >> arr[i]; 
+		cin >> arr[i];
 
 	}
 	for (i = 0; i < 5; i++) {
 		cout << "Data Array ke-" << i << " :" << arr[i] << endl;
-	} 
-	return 0; 
+	}
+	return 0;
+} 
+
+
